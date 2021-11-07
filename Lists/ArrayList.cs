a11f3b600bbbea1620cd0b30addbe45f9dfab304
galimova asya <alimgafarova@gmail.com>
2021-11-08 01:45:47 +0500

    Add ArrayList+ArrayTest

---
+++ Lists/ArrayList.cs
@@ -0,0 +1,395 @@
+﻿using System;
+
+namespace Lists
+{
+	public class ArrayList
+	{
+		private const int _minArrayLength = 10;
+
+		private int[] _array; //для меня
+
+		public int Length { get; private set; } //пользовательский (количество элементов в массиве)
+
+		public ArrayList()
+		{
+			Length = 0;
+			_array = new int[_minArrayLength];
+		}
+
+		public ArrayList(int[] array)
+		{
+			_array = new int[(int)(array.Length * 1.5 + 1)];
+			Array.Copy(array, _array, array.Length);
+			Length = array.Length;
+		}
+
+		//1. добавление значения в конец
+		public void AddElementToEnd(int value)
+		{
+			if (Length == _array.Length)
+			{
+				IncreaseArraySize();
+			}
+			_array[Length] = value;
+			Length++;
+		}
+
+		// 2. добавление значения в начало
+		public void AddToBeginning(int value)
+		{
+			if (Length == _array.Length)
+			{
+				IncreaseArraySize();
+			}
+			ShiftRight();
+			Length++;
+			_array[0] = value;
+		}
+
+		//3. добавление значения по индексу
+		public void AddElementByIndex(int value, int index)
+		{
+			if (Length == _array.Length)
+			{
+				IncreaseArraySize();
+			}
+			ShiftRight(index);
+			Length++;
+			_array[index] = value;
+		}
+
+		//4. удаление из конца одного элемента
+		public void RemoveLastElement()
+		{
+			_array[Length - 1] = 0;
+			Length--;
+		}
+
+		//5. удаление из начала одного элемента
+		public void RemoveFirstElement()
+		{
+			_array[0] = 0;
+			ShiftLeft();
+			Length--;
+		}
+
+		//6. удаление по индексу одного элемента
+		public void RemoveAtIndex(int index)
+		{
+			_array[index] = 0;
+			ShiftLeft(index);
+			Length--;
+		}
+
+		//7. удаление из конца N элементов 
+		public void RemoveLastNTimes(int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				RemoveLastElement();
+			}
+		}
+
+		//8. удаление из начала N элементов
+		public void RemoveFirstNTimes(int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				RemoveFirstElement();
+			}
+		}
+
+		//9.удаление по индексу N элементов 
+		public void RemoveAtIndexNTimes(int n, int index)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				RemoveAtIndex(index);
+			}
+		}
+
+		//10. вернуть длину
+		public int GetLength()
+		{
+			int length = 0;
+			length = Length;
+			return length;
+		}
+
+		//11. доступ по индексу
+		public int GetIndexAcsess(int index)
+		{
+			int value = 0;
+			value = _array[index];
+			return value;
+		}
+
+		//12. первый индекс по значению
+		public int GetFirstIndexByValue(int value)
+		{
+			int index = 0;
+			for (int i = 0; i < Length; i++)
+			{
+				if (value == _array[i])
+				{
+					index = i;
+					return index;
+				}
+			}
+			return -1;
+		}
+
+		//13. изменение по индексу 
+		public void GetChangeByIndex(int index, int value)
+		{
+			int tmp = 0;
+			tmp = _array[index];
+			_array[index] = value;
+			value = tmp;
+        }
+
+		//14.реверс
+        public void GetReverse()
+        {
+            int x = 0;
+			int y = Length - 1;
+			while (x < y)
+            {
+                int tmp = _array[x];
+                _array[x] = _array[y];
+                _array[y] = tmp;
+                x++;
+				y--;
+			}
+        }
+
+        //15. поиск значения максимального элемента
+        public int GetMaximumElement()
+		{
+			int max = _array[0];
+			for (int i = 0; i < Length; i++)
+			{
+				if (_array[i] > max)
+				{
+					max = _array[i];
+				}
+			}
+			return max;
+		}
+		//16. поиск значения минимального элемента
+		public int GetMinimumElement()
+		{
+			int min = _array[0];
+			for (int i = 0; i < Length; i++)
+			{
+				if (_array[i] < min)
+				{
+					min = _array[i];
+				}
+			}
+			return min;
+		}
+		//17. поиск индекс максимального элемента
+		public int GetIndexMaximumElement()
+		{
+            int max = _array[0];
+            int indexMax = 0;
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] > max)
+                {
+                    max = _array[i];
+                    indexMax = i;
+                }
+            }
+            return indexMax;
+        }
+		//18. поиск индекс минимального элемента
+		public int GetIndexMinimumElement()
+		{
+			int min = _array[0];
+			int indexMin = 0;
+			for (int i = 0; i < Length; i++)
+			{
+				if (_array[i] < min)
+				{
+					min = _array[i];
+					indexMin = i;
+				}
+			}
+			return indexMin;
+		}
+		//19. сортировка по возрастанию
+		public void GetSortAscerding()
+		{
+			int tmp = 0;
+			for (int i = 0; i < Length - 1; i++)
+			{
+				for (int j = 0; j < Length - i - 1; j++)
+				{
+					if (_array[j] > _array[j + 1])
+					{
+						tmp = _array[j];
+						_array[j] = _array[j + 1];
+						_array[j + 1] = tmp;
+					}
+				}
+			}
+		}
+		//20. сортировка по убыванию
+		public void GetSortDescending()
+		{
+			int tmp = 0;
+			for (int i = 0; i < Length; i++)
+			{
+				for (int j = i + 1; j < Length; j++)
+				{
+					if (_array[i] < _array[j])
+					{
+						tmp = _array[i];
+						_array[i] = _array[j];
+						_array[j] = tmp;
+					}
+				}
+			}
+		}
+		//21. удаление по значению первого(?вернуть индекс) -удали первую 7= первую семерку котоорую он встретит удаляетБ если нет -1
+		public void RemoveByValueOfFirst(int value)
+		{
+			for (int i = 0; i < Length; i++)
+			{
+				if (_array[i] == value)
+				{
+					_array[i] = 0;
+					ShiftLeft(i);
+					Length--;
+				}
+			}
+		}
+		//22. удаление по значению всех(?вернуть кол-во)- удаление всех 7 в массиве
+		public void RemoveAllValue(int value)
+		{
+			int count = 0;
+			for (int i = 0; i < Length; i++)
+			{
+				if (_array[i] == value)
+				{
+					_array[i] = 0;
+					count++;
+					ShiftLeft(i);
+					Length--;
+				}
+			}
+		}
+		//23. 3 конструктора(пустой, на основе одного элемента, на основе массива ) -  метод добавить по индексу
+		//24. добавление списка(вашего самодельного) в конец
+		//25. добавление списка в начало
+		//26. добавление списка по индексу
+
+
+		//увеличение размера массива
+		private void IncreaseArraySize()
+		{
+			int[] tmpArray = new int[(int)(_array.Length * 1.5)];
+			for (int i = 0; i < Length; i++)
+			{
+				tmpArray[i] = _array[i];
+			}
+			_array = tmpArray;
+		}
+
+		//уменьшение размера массива
+		private void DescreaseArraySize()
+		{
+			if (Length < _array.Length)
+			{
+				int[] tmpArray = new int[(int)(Length * 0.75)];
+				for (int i = 0; i < Length; i++)
+				{
+					tmpArray[i] = _array[i];
+				}
+				_array = tmpArray;
+			}
+		}
+
+		// Смещает элементы массива с определённой позиции вправо на единицу
+		private void ShiftRight(int position = 0)
+		{
+			// Проверка position
+			if (position >= Length)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			while (Length == _array.Length)
+			{
+				IncreaseArraySize();
+			}
+			for (int i = Length - 1; i >= position; i--)
+			{
+				_array[i + 1] = _array[i];
+			}
+			for (int i = position; i <= position; i++)
+			{
+				_array[i] = 0;
+			}
+		}
+
+		// Смещает элементы массива с определённой позиции влево на единицу
+		private void ShiftLeft(int position = 0)
+		{
+			if (position < 0)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			for (int i = position + 1; i <= Length - 1; i++)
+			{
+				_array[i - 1] = _array[i];
+			}
+			_array[Length - 1] = 0;
+		}
+
+		public void WriteToConsole() //для консоли
+		{
+			for (int i = 0; i < Length; i++)
+			{
+				Console.Write($"{_array[i]} ");
+			}
+			Console.WriteLine();
+		}
+
+		public override bool Equals(object obj)
+		{
+			ArrayList arrayList = (ArrayList)obj;
+
+			if (Length != arrayList.Length)
+			{
+				return false;
+			}
+
+			for (int i = 0; i < Length; i++)
+			{
+				if (arrayList._array[i] != _array[i])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public override string ToString()
+		{
+			string s = "";
+			for (int i = 0; i < Length; i++)
+			{
+				s += $"{_array[i]} ";
+			}
+			return s;
+		}
+
+		public override int GetHashCode()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ ListsTests/ArrayListTest.cs
@@ -0,0 +1,389 @@
+﻿using NUnit.Framework;
+using Lists;
+namespace ListsTests
+{
+    public class ArrayTests
+    {
+        //1
+        #region AddElementToEndTest
+        [TestCase(1, new int[] { 1, 1, 1 }, new int[] { 1, 1, 1, 1 })]
+        [TestCase(1, new int[] { }, new int[] { 1 })]
+        [TestCase(-1, new int[] { 1, 1 }, new int[] { 1, 1, -1 })]
+        public void AddElementToEndTest(int value, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.AddElementToEnd(value);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //2
+        #region AddToBeginningTest
+        [TestCase(1, new int[] { 1, 1, 1 }, new int[] { 1, 1, 1, 1 })]
+        //[TestCase(1, new int[] { }, new int[] { 1 })]
+        [TestCase(-1, new int[] { 1, 1 }, new int[] { -1, 1, 1 })]
+        [TestCase(-1, new int[] { 9, 5, 3, 6, 7 }, new int[] { -1, 9, 5, 3, 6, 7 })]
+        public void AddToBeginningTest(int value, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.AddToBeginning(value);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //3
+        #region AddElementByIndexTest
+        [TestCase(5, 1, new int[] { 1, 1, 1 }, new int[] { 1, 5, 1, 1 })]
+        //[TestCase(5, 1, new int[] { }, new int[] { 0, 5 })]
+        [TestCase(5, 1, new int[] { 1, 1 }, new int[] { 1, 5, 1 })]
+        [TestCase(5, 4, new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5, 3, 6, 5, 7 })]
+        public void AddElementByIndexTest(int value, int index, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.AddElementByIndex(value, index);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //4
+        #region RemoveLastElementTest
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1 })]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1, 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5, 3, 6 })]
+        public void RemoveLastElementTest(int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveLastElement();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //5
+        #region RemoveFirstElementTest
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1 })]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1, 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, new int[] { 5, 3, 6, 7 })]
+        public void RemoveFirstElementTest(int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveFirstElement();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //6
+        #region RemoveAtIndexTest
+        [TestCase(0, new int[] { 1, 1, 1 }, new int[] { 1, 1 })]
+        //[TestCase(0, new int[] { }, new int[] { })]
+        [TestCase(0, new int[] { 1, 1 }, new int[] { 1 })]
+        [TestCase(2, new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5, 6, 7 })]
+        public void RemoveAtIndexTest(int index, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveAtIndex(index);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //7
+        #region RemoveLastNTimesTest
+        [TestCase(4, new int[] { 1, 1, 1, 1, 1, }, new int[] { 1 })]
+        //[TestCase(0, new int[] { }, new int[] { })]
+        [TestCase(2, new int[] { 1, 1 }, new int[] {  })]
+        [TestCase(3, new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5 })]
+        public void RemoveLastNTimesTest(int n, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveLastNTimes(n);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //8
+        #region RemoveFirstNTimesTest
+        [TestCase(4, new int[] { 1, 1, 1, 1, 1, }, new int[] { 1 })]
+        //[TestCase(0, new int[] { }, new int[] { })]
+        [TestCase(2, new int[] { 1, 1 }, new int[] { })]
+        [TestCase(3, new int[] { 9, 5, 3, 6, 7 }, new int[] { 6, 7 })]
+        public void RemoveFirstNTimesTest(int n, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveFirstNTimes(n);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //9
+        #region RemoveAtIndexNTimesTest
+        [TestCase(2, 0, new int[] { 1, 1, 1, 1, 1, }, new int[] { 1, 1, 1, })]
+        //[TestCase(0, new int[] { }, new int[] { })]
+        [TestCase(2, 1, new int[] { 1, 1 }, new int[] { })]
+        [TestCase(3, 2, new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5 })]
+        public void RemoveAtIndexNTimesTest(int n, int index, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveAtIndexNTimes(n, index);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //10
+        #region GetLengthTest
+        [TestCase(new int[] { 1, 1, 1 }, 3)]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { -1, 1 }, 2)]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, 5)]
+        public void GetLengthTest(int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetLength();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //11
+        #region GetIndexAcsessTest
+        [TestCase(2, new int[] { 1, 1, 1 }, 1)]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(0, new int[] { -1, 1 }, -1)]
+        [TestCase(3, new int[] { 9, 5, 3, 6, 7 }, 6)]
+        public void GetIndexAcsessTest(int index, int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetIndexAcsess(index);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //12
+        #region GetFirstIndexByValueTest
+        [TestCase(1, new int[] { 1, 1, 1 }, 0)]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(-1, new int[] { -1, 1, -1 }, 0)]
+        [TestCase(3, new int[] { 9, 5, 3, 6, 7 }, 2)]
+        public void GetFirstIndexByValueTest(int value, int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetFirstIndexByValue(value);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //13
+        #region GetChangeByIndexTest
+        [TestCase(0, 2, new int[] { 1, 1, 1, 1, 1, }, new int[] { 2, 1, 1, 1, 1, })]
+        //[TestCase(0, new int[] { }, new int[] { })]
+        [TestCase(1, 2, new int[] { 1, 1 }, new int[] { 1, 2 })]
+        [TestCase(3, 2, new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5, 3, 2, 7 })]
+        public void GetChangeByIndexTest(int index, int value, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.GetChangeByIndex(index, value);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //14
+        #region GetReverseTest
+        [TestCase(new int[] { 1, 1, 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1, 1 })]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 2, 1 }, new int[] { 1, 2 })]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, new int[] { 7, 6, 3, 5, 9 })]
+        public void GetReverseTest(int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.GetReverse();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //15
+        #region GetMaximumElementTest
+        [TestCase(new int[] { 1, 1, 1 }, 1)]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { -1, 1 }, 1)]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, 9)]
+        public void GetMaximumElementTest(int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetMaximumElement();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //16
+        #region GetMinimumElementTest
+        [TestCase(new int[] { 1, 1, 1 }, 1)]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { -1, 1 }, -1)]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, 3)]
+        public void GetMinimumElementTest(int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetMinimumElement();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //17
+        #region GetIndexMaximumElementTest
+        [TestCase(new int[] { 1, 1, 1, 1, 1, }, 0)]
+        //[TestCase(new int[] { }, )]
+        [TestCase(new int[] { -1, 1 }, 1)]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, 0)]
+        public void GetIndexMaximumElementTest(int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetIndexMaximumElement();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //18
+        #region GetIndexMinimumElementTest
+        [TestCase(new int[] { 1, 1, 1, 1, 1, }, 0)]
+        //[TestCase(new int[] { }, )]
+        [TestCase(new int[] { -1, 1 }, 0)]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, 2)]
+        public void GetIndexMinimumElementTest(int[] array, int expected)
+        {
+            ArrayList _array = new ArrayList(array);
+            int actual = _array.GetIndexMinimumElement();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //19
+        #region GetSortAscerdingTest
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 })]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 0, 2, -1 }, new int[] { -1, 0, 2 })]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, new int[] { 3, 5, 6, 7, 9})]
+        public void GetSortAscerdingTest(int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.GetSortAscerding();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //20
+        #region GetSortDescendingTest
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 })]
+        //[TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 2, -1, 0 }, new int[] { 2, 0, -1 })]
+        [TestCase(new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 7, 6, 5, 3 })]
+        public void GetSortDescendingTest(int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.GetSortDescending();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //21
+        #region RemoveByValueOfFirstTest
+        [TestCase(3, new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 })]
+        [TestCase(1, new int[] { }, new int[] { })]
+        [TestCase(-1, new int[] { -1, 1 }, new int[] { 1 })]
+        [TestCase(3, new int[] { 9, 5, 3, 6, 7 }, new int[] { 9, 5, 6, 7 })]
+        public void RemoveByValueOfFirstTest(int value, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveByValueOfFirst(value);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+        //22
+        #region RemoveAllValueTest
+        //[TestCase(1, new int[] { 1, 1, 1 }, new int[] { })]
+        [TestCase(1, new int[] { }, new int[] { })]
+        [TestCase(-1, new int[] { -1, 1, -1, 1}, new int[] { 1, 1 })]
+        [TestCase(7, new int[] { 9, 5, 7, 3, 6, 7 }, new int[] { 9, 5, 3, 6 })]
+        public void RemoveAllValueTest(int value, int[] array, int[] expectedArray)
+        {
+            ArrayList actual = new ArrayList(array);
+            ArrayList expected = new ArrayList(expectedArray);
+
+            actual.RemoveAllValue(value);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        #endregion
+
+    }
+}
